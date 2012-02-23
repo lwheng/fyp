f4db2b276c4d89363b6e5ae19eb769681cb15e3b
Low-Wee Heng <alpha116@gmail.com>
2012-02-23 15:21:40 +0800

    work in progress

--- code/vocab.py
+++ code/vocab.py
@@ -1,2 +1,56 @@
 # This script takes in a set of documents and generate the set of the words
 # found in the documents.
+
+import os
+import sys
+import getopt
+from datetime import datetime
+
+date = str(datetime.now().date())
+time = str(datetime.now().time())
+
+fileDirectory = ""
+vocabFile = "/Users/lwheng/Desktop/vocab-(" + date + "-" + time + ").txt"
+
+def vocab():
+	global fileDirectory
+	global vocabFile
+
+	for dirname, dirnames, filenames in os.walk(fileDirectory):
+		for filename in filenames:
+			print os.path.join(dirname, filename)
+
+	print vocabFile
+
+
+def usage():
+	print "USAGE: python " + sys.argv[0] +" -d <fileDirectory>"
+	print "Default output location is Desktop, vocab-(timestamp).txt"
+	print "To specify output, add this: -o <output filename>"
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "d:o:")
+        for opt, args in opts:
+            if opt == "-d":
+                global fileDirectory
+                fileDirectory = args
+            elif opt == "-o":
+                global vocabFile
+                vocabFile = args
+    except getopt.GetoptError:
+    	usage()
+    	sys.exit(2)
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
+	vocab()
+
+
+
+
+
+
+
+
+
