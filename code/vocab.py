902d7122a13624dc8b510b26ac180eb01f1715be
Low-Wee Heng <alpha116@gmail.com>
2012-02-24 23:05:20 +0800

    isalnum, hyphenated, apos, remove punctuation

--- code/vocab.py
+++ code/vocab.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import string
 import getopt
 from nltk.stem.wordnet import WordNetLemmatizer
 from datetime import datetime
@@ -15,15 +16,60 @@ vocabFile = "/Users/lwheng/Desktop/vocab-(" + date + "-" + time + ").txt"
 
 lmtzr = WordNetLemmatizer()
 
+def hyphenated(word):
+    tokens = word.split("-")
+    for t in tokens:
+        if not t.isalnum():
+            return False
+    return True
+
+def apos(word):
+    tokens = word.split("'")
+    if len(tokens) != 2:
+        return False
+    for t in tokens:
+        if not t.isalnum():
+            return False
+    return True
+
+def removepunctuation(word):
+    output = ""
+    for w in word:
+        if not w in string.punctuation:
+            output += w
+    return output
+
 def vocab():
     global fileDirectory
     global vocabFile
     global lmtzr
+
+    vocabDict = {}
     
     for dirname, dirnames, filenames in os.walk(fileDirectory):
         for filename in filenames:
             thefile = str(os.path.join(dirname, filename))
             openthefile = open(thefile, "r")
+            for l in openthefile:
+                line = l[:-1]
+                line = line.lower()
+                tokens = line.split()
+                for t in tokens:
+                    toadd = ""
+                    if t.isalnum() or hyphenated(t) or apos(t):
+                        toadd = t
+                    elif removepunctuation(t).isalnum():
+                        toadd = removepunctuation(t)
+
+                    if len(toadd) != 0:
+                        if toadd not in vocabDict:
+                            vocabDict[toadd] = 0
+                        vocabDict[toadd] += 1
+
+    openvocabfile = open(vocabFile,"w")
+    for k in vocabDict:
+        towrite = k + "\n"
+        openvocabfile.write(towrite)
 
 def usage():
 	print "USAGE: python " + sys.argv[0] +" -d <fileDirectory>"
