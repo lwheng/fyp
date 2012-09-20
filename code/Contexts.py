c1ecdba6893611d03209e9d56ec30a6f75fd66fb
Low-Wee Heng <alpha116@gmail.com>
2012-09-21 02:19:09 +0800

    debuggin

--- code/Contexts.py
+++ code/Contexts.py
@@ -35,6 +35,8 @@ if __name__ == '__main__':
     if len(title_to_match) == 0:
       print dom_parscit_section_cited.toxml()
       sys.exit()
+    else:
+      title_to_match = title_to_match[0].firstChild.wholeText
 
     # Citations in citing
     citations = dom_parscit_citing.getElementsByTagName('citation')
