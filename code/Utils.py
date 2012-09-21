bcec3d9c45bb607ebbe9f663f1e1258af8ff0a40
Low-Wee Heng <alpha116@gmail.com>
2012-09-21 19:23:32 +0800

    debug

--- code/Utils.py
+++ code/Utils.py
@@ -141,9 +141,8 @@ class weight:
     docs = []
     body_texts = dom_parscit_section_cited.getElementsByTagName('bodyText')
     for body_text in body_texts:
-      whole_text = body_text.firstChild.wholeText
-      print type(whole_text)
-      whole_text = str(whole_text.lower())
+      whole_text = body_text.firstChild.wholeText.lower()
+      whole_text = unicode(whole_text.encode('ascii', 'ignore'), errors='ignore')
       text = self.nltk_tools.nltk_text(self.nltk_tools.nltk_word_tokenize(whole_text.lower()))
       docs.append(text)
     docs_col = self.nltk_tools.nltk_text_collection(docs)
