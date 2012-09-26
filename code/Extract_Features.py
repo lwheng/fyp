62f7b56bc242136321aff94ffef34432fd7b106c
Low-Wee Heng <alpha116@gmail.com>
2012-09-27 02:18:01 +0800

    some annotations

--- code/Extract_Features.py
+++ code/Extract_Features.py
@@ -47,4 +47,5 @@ if __name__ == "__main__":
   big_X = np.asarray(big_X)
   
   # Dump pickle
-  pickle.dump(big_X, open(os.path.join(path_pickles,'Big_X.pickle'),'wb'))
+  #pickle.dump(big_X, open(os.path.join(path_pickles,'Big_X.pickle'),'wb'))
+  pickle.dump(big_X, open(os.path.join(path_pickles,'Big_X_With_Publish_Year.pickle'),'wb'))
