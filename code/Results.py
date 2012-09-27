af32594d5c0841a9ec6bd58277432c5bce825881
Low-Wee Heng <alpha116@gmail.com>
2012-09-28 03:00:15 +0800

    features

--- code/Results.py
+++ code/Results.py
@@ -1,6 +1,7 @@
 import cPickle as pickle
 import os, sys
 import numpy as np
+import pylab as pl
 from sklearn import svm, metrics
 from sklearn import cross_validation
 from sklearn.linear_model import LogisticRegression
@@ -8,6 +9,11 @@ from sklearn.cross_validation import KFold
 from sklearn.cross_validation import LeaveOneOut
 from sklearn.cross_validation import LeavePOut
 from sklearn.feature_selection import RFE
+from sklearn.feature_selection import SelectPercentile, f_classif
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.feature_selection import RFECV
+from sklearn.datasets import make_classification
+from sklearn.metrics import zero_one
 
 if __name__ == "__main__":
   # Load Config.pickle
@@ -54,11 +60,35 @@ if __name__ == "__main__":
   clf.fit(X_train, y_train)
   expected = y_test
   predicted = clf.predict(X_test)
+
+  print
   print "Feature Ranking with Recursive Feature Elimination"
   selector = RFE(clf, 1, step=1)
   selector = selector.fit(X, y)
   print selector.support_
   print selector.ranking_
+  print
+
+  ## Build a classification task using 3 informative features
+  #X, y = make_classification(n_samples=y.shape[0], n_features=X.shape[1], n_informative=3,
+  #        n_redundant=2, n_repeated=0, n_classes=2, n_clusters_per_class=1,
+  #                random_state=0)
+
+  ## Create the RFE object and compute a cross-validated score.
+  #svc = svm.SVC(kernel="linear")
+  #rfecv = RFECV(estimator=svc, step=1, cv=StratifiedKFold(y, 2),
+  #        loss_func=zero_one)
+  #rfecv.fit(X, y)
+
+  #print "Optimal number of features : %d" % rfecv.n_features_
+
+  ## Plot number of features VS. cross-validation scores
+  #pl.figure()
+  #pl.xlabel("Number of features selected")
+  #pl.ylabel("Cross validation score (nb of misclassifications)")
+  #pl.plot(xrange(1, len(rfecv.cv_scores_) + 1), rfecv.cv_scores_)
+  #pl.show()
+
   print "X_train" + str(X_train.shape)
   print "X_test" + str(X_test.shape)
   print "y_train" + str(y_train.shape)
