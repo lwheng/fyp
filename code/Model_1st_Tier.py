834329450bea52232bbc5155ca589f0c1cab9546
Low-Wee Heng <alpha116@gmail.com>
2012-10-16 13:18:20 +0800

    skeleton of 1st

--- code/Model_1st_Tier.py
+++ code/Model_1st_Tier.py
@@ -1,4 +1,7 @@
+execfile("Utils.py")
+import Utils
 import cPickle as pickle
+import os
 
 if __name__ == "__main__":
   print "#######################"
@@ -12,7 +15,7 @@ if __name__ == "__main__":
   path_pickles = config['path_pickles']
 
   ## Input To Model
-  # 1. Contexts
+  # Contexts
   print "----------------"
   print "Loading Contexts"
   print "----------------"
@@ -22,7 +25,30 @@ if __name__ == "__main__":
   print "---------------------------------"
   feature_extractor = Utils.extract_features()
 
-  # 2. Annotations
+  # Annotations
   print "-------------------"
   print "Loading Annotations"
   print "-------------------"
+  # Run Targets.py to generate Y.pickle from Labels.txt
+  execfile("Targets.py")
+  # Load y_raw
+  y_raw = pickle.load(open(os.path.join(path_pickles, 'Y.pickle'),'r'))
+  y_info_raw = pickle.load(open(os.path.join(path_pickles, 'Y_Info.pickle'),'r'))
+  print "------------------"
+  print "Loaded Annotations"
+  print "------------------"
+
+  # Pre-processing on X and y
+
+  ## Fit the Model
+  print "-----------------"
+  print "Fitting the Model"
+  print "-----------------"
+  # Select Classifier
+  # Fit X and y
+  print "-----------------------------"
+  print "Model Fitted. Writing out now"
+  print "-----------------------------"
+
+  ## Write out the Model
+  pickle.dump(model, open(os.path.join(path_pickles, 'Model_1st_Tier.pickle'),'wb'))
