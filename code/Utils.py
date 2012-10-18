0f05762414d75754a5f0db7ee7235157fbbd17c4
Low-Wee Heng <alpha116@gmail.com>
2012-10-18 18:07:06 +0800

    trying combinations of features

--- code/Utils.py
+++ code/Utils.py
@@ -685,12 +685,12 @@ class extract_features:
     x.append(feature_publish_year)
 
     # Title Overlap
-    #feature_title_overlap = self.weight.title_overlap(dom_parscit_section_citing, dom_parscit_section_cited)
+    feature_title_overlap = self.weight.title_overlap(dom_parscit_section_citing, dom_parscit_section_cited)
     #x.append(feature_title_overlap)
 
-    # Authors Overlap
+     Authors Overlap
     #feature_author_overlap = self.weight.author_overlap(dom_parscit_section_citing, dom_parscit_section_cited)
-    #x.append(feature_author_overlap)
+    x.append(feature_author_overlap)
 
     # Context's Average TF-IDF Weight
     feature_query_weight = self.weight.chunk_average_weight(query_text, citing_col)
