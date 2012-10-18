ddc2164c3983fe6ea09e895f3d0ae379cbb50150
Low-Wee Heng <alpha116@gmail.com>
2012-10-18 17:49:07 +0800

    trying combinations of features

--- code/Utils.py
+++ code/Utils.py
@@ -673,12 +673,12 @@ class extract_features:
     x = []
     
     # Citation Density
-    #feature_cit_density = self.weight.cit_density(query, cit_str)
-    #x.append(feature_cit_density)
+    feature_cit_density = self.weight.cit_density(query, cit_str)
+    x.append(feature_cit_density)
 
     # Number Density
-    feature_num_density = self.weight.number_density(query)
-    x.append(feature_num_density)
+    #feature_num_density = self.weight.number_density(query)
+    #x.append(feature_num_density)
 
     # Publishing Year Difference
     #feature_publish_year = self.dist.publish_year(f)
