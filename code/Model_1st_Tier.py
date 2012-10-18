606ca18f47355070c5d91404853d077f1c56dec1
Low-Wee Heng <alpha116@gmail.com>
2012-10-18 17:08:26 +0800

    debug

--- code/Model_1st_Tier.py
+++ code/Model_1st_Tier.py
@@ -131,6 +131,6 @@ if __name__ == "__main__":
   print X_train
   print y_train
   clf = svm.SVC(kernel='linear')
-  clf.fit(X, y)
+  clf.fit(X_train, y_train)
   predicted = clf.predict(X_train)
   print predicted
