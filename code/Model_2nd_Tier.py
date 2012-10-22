79bd3bc08f9a775e81401d3c7b34ff1879217622
Low-Wee Heng <alpha116@gmail.com>
2012-10-22 23:28:28 +0800

    setting up flow for 2nd

--- code/Model_2nd_Tier.py
+++ code/Model_2nd_Tier.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
       for i in range(len(x)):
         if i in c_hash:
           # Append y
-          print "YES"
+          print "c_hash = " + str(c_hash) + ", i = " + str(i)
           y.append(c_hash[i])
         else:
           # Append n
