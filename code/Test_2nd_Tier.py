584fc9137a5b3c246fdc3cfcd771e94862479bce
Low-Wee Heng <alpha116@gmail.com>
2012-10-23 16:47:01 +0800

    adding features to 2nd tier

--- code/Test_2nd_Tier.py
+++ code/Test_2nd_Tier.py
@@ -11,4 +11,44 @@ from sklearn import cross_validation
 if __name__ == "__main__":
   # Load Model_2nd_Tier
   model_2nd_tier = pickle.load(open(os.path.join(path_pickles, 'Model_2nd_Tier.pickle'), 'r'))
-  print model_2nd_tier
+  # Load X
+  X = pickle.load(open(os.path.join(path_pickles, 'X_2nd_Tier.pickle'), 'r'))
+  # Load y
+  y = pickle.load(open(os.path.join(path_pickles, 'y_2nd_Tier.pickle'), 'r'))
+
+  # Prediction
+  X_n = []
+  y_n = []
+  X_y = []
+  y_y = []
+  for i in range(y.shape[0]):
+    temp_x = list(X[i])
+    temp_y = int(y[i])
+    if temp_y == 1:
+      X_y.append(temp_x)
+      y_y.append(temp_y)
+    else:
+      X_n.append(temp_x)
+      y_n.append(temp_y)
+  X_train = X_y
+  y_train = y_y
+  
+  # Pick randomly
+  times = 2
+  X_train.extend(random.sample(X_g, int(len(y_train)*times)))
+  y_train.extend(random.sample(y_g, int(len(y_train)*times)))
+  
+  X_train = np.asarray(X_train)
+  y_train = np.asarray(y_train)
+  print X_train.shape
+  print y_train
+  #clf = svm.SVC(kernel='linear')
+  clf = svm.SVC()
+  #clf.fit(X, y)
+  #predicted = clf.predict(X_train)
+  #expected = y_train
+  clf.fit(X_train, y_train)
+  predicted = clf.predict(X)
+  expected = y
+  print "Classification report for classifier %s:\n%s\n" % (clf, metrics.classification_report(expected, predicted))
+  print "Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted)
