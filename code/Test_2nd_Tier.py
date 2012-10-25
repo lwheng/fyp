3a976daaa1474f2700e6ed1382e8d6e00d0cf7f0
Low-Wee Heng <alpha116@gmail.com>
2012-10-25 12:00:07 +0800

    setting up testing for 2ndtier

--- code/Test_2nd_Tier.py
+++ code/Test_2nd_Tier.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     Xy_n.append((X_n[i], y_n[i]))
 
   # Pick randomly from Xy_n
-  times = 3
+  times = 1
   sample = random.sample(Xy_n, int(len(y_train)*times))
   for (d, a) in sample:
     X_train.append(d)
