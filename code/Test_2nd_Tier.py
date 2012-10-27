f113cbbf90084de5be821e40d9b55fdd0186ffdb
Low-Wee Heng <alpha116@gmail.com>
2012-10-27 22:22:34 +0800

    tier

--- code/Test_2nd_Tier.py
+++ code/Test_2nd_Tier.py
@@ -55,71 +55,57 @@ if __name__ == "__main__":
     y_train.append(a)
   
   # Perform leave-one-out evaluation
-  # Perform 10 rounds and compute the average
-  rounds = 10
-  svm_score = float(0)
-  nb_score = float(0)
-  dt_score = float(0)
-  while (rounds > 0):
-    total = len(X_train)
-    svm_correct = 0
-    nb_correct = 0
-    dt_correct = 0
-    rounds -= 1
-    for i in range(len(X_train)):
-      front_X = X_train[:i]
-      test_X = X_train[i]
-      back_X = X_train[i+1:]
-      
-      front_y = y_train[:i]
-      test_y = y_train[i]
-      back_y = y_train[i+1:]
+  total = len(X_train)
+  correct = 0
+  svm_predicted = []
+  svm_expected = []
+  naivebayes_predicted = []
+  naivebayes_expected = []
+  decisiontree_predicted = []
+  decisiontree_expected = []
+  for i in range(len(X_train)):
+    front_X = X_train[:i]
+    test_X = X_train[i]
+    back_X = X_train[i+1:]
+    
+    front_y = y_train[:i]
+    test_y = y_train[i]
+    back_y = y_train[i+1:]
 
-      training_X = front_X + back_X
-      training_y = front_y + back_y
-      training_X = np.asarray(training_X)
-      training_y = np.asarray(training_y)
+    training_X = front_X + back_X
+    training_y = front_y + back_y
+    training_X = np.asarray(training_X)
+    training_y = np.asarray(training_y)
 
-      clf = svm.SVC()
-      clf.fit(training_X, training_y)
-      predicted = clf.predict(test_X)
-      expected = test_y
-      if predicted == expected:
-        svm_correct += 1
-      
-      clf = GaussianNB()
-      clf.fit(training_X, training_y)
-      predicted = clf.predict(test_X)
-      expected = test_y
-      if predicted == expected:
-        nb_correct += 1
-      
-      clf = DecisionTreeClassifier()
-      clf.fit(training_X, training_y)
-      predicted = clf.predict(test_X)
-      expected = test_y
-      if predicted == expected:
-        dt_correct += 1
-    svm_score += float(svm_correct) / float(total)
-    nb_score += float(nb_correct) / float(total)
-    dt_score += float(dt_correct) / float(total)
-  print "SVM = " + str(svm_score / float(10))
-  print "NB = " + str(nb_score / float(10))
-  print "DT = " + str(dt_score / float(10))
-  print
-  
-  sys.exit()
-  X_train = np.asarray(X_train)
-  y_train = np.asarray(y_train)
-  print X_train.shape
-  print y_train
-  #clf = svm.SVC(kernel='linear')
-  clf = svm.SVC()
-  #clf.fit(X, y)
-  #predicted = clf.predict(X_train)
-  #expected = y_train
-  clf.fit(X_train, y_train)
-  predicted = clf.predict(X_train)
-  expected = y_train
-  print "Classification report for classifier %s:\n%s\n" % (clf, metrics.classification_report(expected, predicted))
-  print "Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted)
+    clf = svm.SVC()
+    clf.fit(training_X, training_y)
+    predicted = clf.predict(test_X)
+    expected = test_y
+    svm_predicted.append(int(predicted[0]))
+    svm_expected.append(int(expected))
+    
+    clf = GaussianNB()
+    clf.fit(training_X, training_y)
+    predicted = clf.predict(test_X)
+    expected = test_y
+    naivebayes_predicted.append(int(predicted[0]))
+    naivebayes_expected.append(int(expected))
+    
+    clf = DecisionTreeClassifier()
+    clf.fit(training_X, training_y)
+    predicted = clf.predict(test_X)
+    expected = test_y
+    decisiontree_predicted.append(int(predicted[0]))
+    decisiontree_expected.append(int(expected))
+  svm_predicted = np.asarray(svm_predicted)
+  svm_expected = np.asarray(svm_expected)
+  print metrics.classification_report(svm_expected, svm_predicted)
+  print "Confusion matrix:\n%s" % metrics.confusion_matrix(svm_expected, svm_predicted)
+  naivebayes_predicted = np.asarray(naivebayes_predicted)
+  naivebayes_expected = np.asarray(naivebayes_expected)
+  print metrics.classification_report(naivebayes_expected, naivebayes_predicted)
+  print "Confusion matrix:\n%s" % metrics.confusion_matrix(naivebayes_expected, naivebayes_predicted)
+  decisiontree_predicted = np.asarray(decisiontree_predicted)
+  decisiontree_expected = np.asarray(decisiontree_expected)
+  print metrics.classification_report(decisiontree_expected, decisiontree_predicted)
+  print "Confusion matrix:\n%s" % metrics.confusion_matrix(decisiontree_expected, decisiontree_predicted)
