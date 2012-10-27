a6615bae72ce9fd407ed0ba4c828449c2001c37f
Low-Wee Heng <alpha116@gmail.com>
2012-10-27 19:18:59 +0800

    1st tier

--- code/Test_1st_Tier.py
+++ code/Test_1st_Tier.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     Xy_g.append((X_g[i], y_g[i]))
 
   # Pick randomly from Xy_g
-  times = 3
+  times = 10
   sample = random.sample(Xy_g, int(len(y_train)*times))
   for (d, a) in sample:
     X_train.append(d)
